41446a9f705f7d16800bf4c2ab3ea54199b72f84
Julien <julienjennequin59123@gmail.com>
2026-03-02 10:06:12 +0100

    refactor: Improve iOS platform code formatting and add locale properties

--- composeApp/src/iosMain/kotlin/org/julienjnnqin/luxmateapp/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/org/julienjnnqin/luxmateapp/Platform.ios.kt
@@ -1,16 +1,20 @@
 package org.julienjnnqin.luxmateapp
 
+import platform.Foundation.NSLocale
+import platform.Foundation.countryCode
+import platform.Foundation.currentLocale
+import platform.Foundation.languageCode
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
+class IOSPlatform : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
 
-actual val myLang:String?
+actual val myLang: String?
     get() = NSLocale.currentLocale.languageCode
 
-actual val myCountry:String?
+actual val myCountry: String?
     get() = NSLocale.currentLocale.countryCode
    
