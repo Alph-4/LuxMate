0f5606fc8fbd31e33141dd7a444b4eb37b7f0159
Alph4 <54032095+Alph-4@users.noreply.github.com>
2026-04-10 10:13:38 +0200

    Initialize Koin for iOS in MainViewController

--- composeApp/src/iosMain/kotlin/org/julienjnnqin/luxmateapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/julienjnnqin/luxmateapp/MainViewController.kt
@@ -1,5 +1,13 @@
 package org.julienjnnqin.luxmateapp
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.julienjnnqin.luxmateapp.di.initializeKoinIos
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        // On initialise Koin avant que le premier écran ne s'affiche
+        initializeKoinIos()
+    }
+) {
+    App()
+}
